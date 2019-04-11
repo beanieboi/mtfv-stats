c39dbc8da3c95a08baf1695b2498bf9660ab718f
ben <beanie@benle.de>
2019-04-11 22:38:22 +0200

    some UI work

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require_relative 'boot'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
-require 'action_text/engine'
 require 'rails/test_unit/railtie'
 require 'sprockets/railtie'
 
