872b00cfa83e0df8d1a30aa595c2729ff74b2187
ben <beanie@benle.de>
2019-04-13 09:35:10 +0200

    Increase limits to 10

--- app/controllers/lifetime_controller.rb
+++ app/controllers/lifetime_controller.rb
@@ -1,8 +1,8 @@
 class LifetimeController < ApplicationController
   def index
-    @singles = LifetimePlayerStats.rank_order("single").limit(3)
-    @doubles = LifetimePlayerStats.rank_order("double").limit(3)
-    @overall = LifetimePlayerStats.rank_order("overall").limit(3)
+    @singles = LifetimePlayerStats.rank_order("single").limit(15)
+    @doubles = LifetimePlayerStats.rank_order("double").limit(15)
+    @overall = LifetimePlayerStats.rank_order("overall").limit(15)
     @pairs = LifetimeDoubleStats.order("overall_performance_index DESC")
   end
 end
--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -5,10 +5,10 @@ class OverviewController < ApplicationController
     @leagues = @season.leagues
     @league = League.find(league_id)
     @teams = @league.rankings
-    @singles = PlayerStats.where(league_id: league_id).rank_order("single").limit(3)
-    @doubles = PlayerStats.where(league_id: league_id).rank_order("double").limit(3)
-    @overall = PlayerStats.where(league_id: league_id).rank_order("overall").limit(3)
-    @pairs = DoubleStats.where(league_id: league_id).order("overall_performance_index DESC").limit(3)
+    @singles = PlayerStats.where(league_id: league_id).rank_order("single").limit(10)
+    @doubles = PlayerStats.where(league_id: league_id).rank_order("double").limit(10)
+    @overall = PlayerStats.where(league_id: league_id).rank_order("overall").limit(10)
+    @pairs = DoubleStats.where(league_id: league_id).order("overall_performance_index DESC").limit(10)
   end
 
   def league_id
