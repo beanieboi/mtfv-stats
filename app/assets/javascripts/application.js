4947fad0def90859fb017e820af4af243c3a6739
Kristin Fritsch <ich@kristin-fritsch.de>
2019-04-18 01:22:30 +0200

    Add search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,69 @@
 //= require Chart.min
+
+var initSearch = function(inputId) {
+  var searchInput;
+  var searchValue;
+  var tableRows;
+
+  // Credit David Walsh (https://davidwalsh.name/javascript-debounce-function)
+
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+  function debounce(func, wait, immediate) {
+    var timeout;
+
+    return function executedFunction() {
+      var context = this;
+      var args = arguments;
+
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+
+      var callNow = immediate && !timeout;
+
+      clearTimeout(timeout);
+
+      timeout = setTimeout(later, wait);
+
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  function search(e) {
+    var newSearchValue = e.srcElement.value.trim().toLowerCase();
+
+    if (searchValue === newSearchValue) {
+      return;
+    }
+
+    searchValue = newSearchValue;
+
+    if (searchValue === "") {
+      tableRows.forEach(function(row) {
+        row.classList.remove("hidden");
+      });
+
+      return;
+    }
+
+    tableRows.forEach(function(row) {
+      row.classList.add("hidden");
+      if (row.textContent.toLowerCase().includes(searchValue)) {
+        row.classList.remove("hidden");
+      }
+    });
+  }
+
+  searchInput = document.getElementById(inputId);
+  tableRows = document.querySelectorAll("tbody tr");
+
+  if (searchInput) {
+    searchInput.addEventListener("input", debounce(search, 1000));
+  } else {
+    console.log("Search was not initialized. ID does not exist");
+  }
+};
