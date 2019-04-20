69bb4e901e507a38781c401eada9baebedde5e13
ben <beanie@benle.de>
2019-04-20 11:30:59 +0200

    Pull out player linking into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def linked_player_names(players)
+    if players.count > 1
+      link_to(players.first.name, player_path(players.first.id)) +
+      " und " +
+      link_to(players.last.name, player_path(players.last.id))
+    else
+      link_to(players.first.name, player_path(players.first.id))
+    end
+  end
 end
