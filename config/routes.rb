5151a5139f376ffc4329b10cbf703ac94d7fe0e9
ben <beanie@benle.de>
2019-04-21 10:25:35 +0200

    put www-redirect at the top

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  # www redirect
+  match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
+
   get 'lifetime', to: 'lifetime#index'
   resources :players, only: [:show]
   resources :teams, only: [:show]
 
   root to: "overview#index"
-
-  # www redirect
-  match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
 end
