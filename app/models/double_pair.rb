349adabb3c95d8bc898b354d22edc56541484cde
ben <beanie@benle.de>
2019-04-22 19:26:59 +0200

    caching to improve stats generation

--- app/models/double_pair.rb
+++ app/models/double_pair.rb
@@ -7,27 +7,31 @@ class DoublePair
   end
 
   def home_results
-    Result.where("home_player_ids = ARRAY[?]", @pair_ids)
+    @home_results ||= Result.where("home_player_ids = ARRAY[?]", @pair_ids)
   end
 
   def away_results
-    Result.where("away_player_ids = ARRAY[?]", @pair_ids)
+    @away_results ||= Result.where("away_player_ids = ARRAY[?]", @pair_ids)
   end
 
   def home_results_in_league(league)
-    Result.in_league(league).where("home_player_ids = ARRAY[?]", @pair_ids)
+    Rails.cache.fetch("home_results_in_league/#{league.id}/#{@pair_ids.join(',')}") do
+      Result.in_league(league).where("home_player_ids = ARRAY[?]", @pair_ids).all.to_a
+    end
   end
 
   def away_results_in_league(league)
-    Result.in_league(league).where("away_player_ids = ARRAY[?]", @pair_ids)
+    Rails.cache.fetch("away_results_in_league/#{league.id}/#{@pair_ids.join(',')}") do
+      Result.in_league(league).where("away_player_ids = ARRAY[?]", @pair_ids).all.to_a
+    end
   end
 
   def leagues
-    matches.collect(&:league).uniq
+    @leagues ||= matches.collect(&:league).uniq
   end
 
   def matches
-    results.includes(:match).collect(&:match)
+    @matches ||= results.includes(:match, match: :league).collect(&:match)
   end
 
   def results
