1b8ae89d907e677ad91daa46c463e419a097ec79
ben <ich@abwesend.com>
2023-04-17 21:23:06 +0200

    no barnes

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,3 @@
-require 'barnes'
-
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -8,10 +6,6 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-before_fork do
-  Barnes.start # Must have enabled worker mode for this to block to be called
-end
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
